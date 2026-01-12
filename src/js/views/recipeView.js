744b06df20f4cdef2751f68eba448eb486ce86e4
bishwash00 <karkibishwash222@gmail.com>
2026-01-12 13:54:49 +0545

    Changes fractional faulty code

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,5 +1,5 @@
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
+import fracty from 'fracty';
 import View from './View.js';
 
 class recipeView extends View {
@@ -110,7 +110,7 @@ class recipeView extends View {
                 <use href="${icons}#icon-check"></use>
               </svg>
               <div class="recipe__quantity">${
-                ing.quantity ? new Fraction(ing.quantity).toString() : ''
+                ing.quantity ? fracty(ing.quantity) : ''
               }</div>
               <div class="recipe__description">
                 <span class="recipe__unit">${ing.unit}</span>
